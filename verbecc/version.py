d472ccf22e8788f2c0554501aceafa883f515850
Brett Tolbert <bretttolbert@gmail.com>
2019-07-17 19:34:14 -0500

    bumped version

--- verbecc/version.py
+++ verbecc/version.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '1.5.3'
+__version__ = '1.5.4'
 __author__ = 'Brett Tolbert'
 __maintainer__ = 'Brett Tolbert'
 __copyright__ = 'Copyright (c) 2019 Brett Tolbert'
