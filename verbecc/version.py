60f021d2124d5f9f819e0b8559f94e98b671f756
Brett Tolbert <bretttolbert@gmail.com>
2021-06-13 12:39:10 -0500

    fixed issues with Portuguese verbs like andar, ficar, amar, odiar and arguir

--- verbecc/version.py
+++ verbecc/version.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '1.6.1'
+__version__ = '1.7.0'
 __author__ = 'Brett Tolbert'
 __maintainer__ = 'Brett Tolbert'
 __copyright__ = 'Copyright (c) 2021 Brett Tolbert'
